2d7e7cede151550c0fcf89578f6fdc9080f38768
codejedi365 <codejedi365@gmail.com>
2021-05-21 23:11:56 -0400

    Project scaffolding

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const isProduction = process.env.NODE_ENV === "production";
+const path = require("path");
+const SheBangPlugin = require("webpack-shebang-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const thisModule = require("./package.json");
+
+function buildConfig(env) {
+  return {
+    entry: "./src/index.ts",
+    target: "node",
+    output: {
+      path: path.resolve(
+        __dirname,
+        path.dirname(thisModule.main) === "."
+          ? "dist"
+          : path.dirname(thisModule.main)
+      ),
+      filename: path.basename(thisModule.main)
+    },
+    plugins: [
+      new SheBangPlugin({
+        chmod: 0o755
+      }),
+      new ESLintPlugin({
+        fix: env.WEBPACK_WATCH
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(ts)$/i,
+          loader: "ts-loader",
+          exclude: ["/node_modules/"],
+        }
+      ],
+    },
+    optimization: {
+      // minimize & mangle the output files (TerserPlugin w/ webpack@v5)
+      minimize: true,
+      // determine which exports are used by modules and removed unused ones
+      usedExports: true
+    },
+    resolve: {
+      extensions: [".ts", ".js", ".json"],
+    },
+  };
+}
+
+module.exports = (env) => {
+  const config = buildConfig(env);
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
