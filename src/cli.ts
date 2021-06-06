49f2562c4df507eb8ae438e9755fb58fd007c475
codejedi365 <codejedi365@gmail.com>
2021-06-06 18:17:13 -0400

    fix(cli): modify module to match cli spec
    Test cases identified problems with the implementation of the cli args
    handler so it had to be adjusted.
    
    yargs doesn't provide the typing information for the helper function
    oddly enough, so added it here.

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,16 @@
-import yargs from "yargs";
+import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
+import { normalize } from "path";
 import thisModule from "../package.json";
 
-type InputArgs = Record<string, unknown> & {
-  _: string[];
+export interface InputArgs {
+  [x: string]: unknown;
+  help?: boolean;
   output: string;
-};
+  infiles: string[];
+  _: string[];
+  $0: string;
+}
 
 /**
  * Function to process command line arguments from the user to parse
@@ -15,14 +20,14 @@ type InputArgs = Record<string, unknown> & {
  * @returns argv as an indexable object defined by yargs
  */
 export function processCLIArgs(argList: string[] = process.argv): InputArgs {
-  return yargs(hideBin(argList))
+  // process.stdout.write(`process.argv = ${JSON.stringify(argList)}\n`);
+  const result = yargs(hideBin(argList))
     .alias("h", "help")
-    .help(
-      "h",
-      "A helper script that merges JSON arrays in multiple files together into one array."
+    .help("help")
+    .usage(
+      "$0 [-o <outfile>] <infiles...>",
+      "Merge GitLab report files into one output file"
     )
-    .usage("Usage: $0 [-o <file>] <files> ...")
-    .version(thisModule.version)
     .options({
       output: {
         alias: "o",
@@ -35,14 +40,30 @@ export function processCLIArgs(argList: string[] = process.argv): InputArgs {
       }
     })
     .check((argv) => {
-      const filePaths = argv._;
+      // process.stdout.write(`argv = ${JSON.stringify(argv)}\n`);
+      if (argv.help) {
+        throw new Error(" "); // tricks Yargs to only printing help info with an extra \n at end.
+      }
+      const filePaths = argv.infiles as InputArgs["infiles"];
       if (filePaths.length < 1) {
         throw new Error("At least 1 input file must be specified.");
       } else {
         return true; // tell Yargs that the arguments passed the check
       }
     })
+    // .middleware((argv) => {
+    //   // Normalize any passed in filepaths
+    //   const filePaths = argv._;
+    //   const normalizedFilePaths = filePaths.map((filePath) =>
+    //     normalize(filePath.toString())
+    //   );
+    //   return { ...argv, _: normalizedFilePaths };
+    // })
+    .version()
+    .describe("version", "show version information")
     .parse() as InputArgs;
+  // process.stdout.write(`result = ${JSON.stringify(result)}\n`);
+  return result;
 }
 
 export default { processCLIArgs };
