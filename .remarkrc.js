6e4c431276e2dd0144752c77d82f1b71b09ce3a0
codejedi365 <codejedi365@gmail.com>
2022-02-21 00:32:43 +0000

    style(conf): convert `remark` conf to js

---
+++ .remarkrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+        "preset-lint-consistent",
+        "preset-lint-markdown-style-guide",
+        "preset-lint-recommended",
+        "preset-prettier"
+    ]
+};
