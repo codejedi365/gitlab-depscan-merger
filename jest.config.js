d7a4d8f29b24bdbe7f9154b4b00e8c7509a6f414
codejedi365 <codejedi365@gmail.com>
2022-02-21 05:03:17 +0000

    test(conf): use `ts-jest` instead

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,43 @@
-module.exports = {
+const tsJestPresets = {
+  preset: "ts-jest",
   globals: {
-    DEV_ENTRYPOINT: "./src/index.ts"
+    DEV_ENTRYPOINT: "./src/index.ts",
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json"
+    }
+  }
+};
+
+module.exports = {
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      lines: 50,
+      statements: 50,
+      functions: 100
+    }
   },
-  setupFilesAfterEnv: ["./test/setupTests.ts"]
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/types/**/*.d.ts",
+    "!**/node_modules/**"
+  ],
+  projects: [
+    {
+      displayName: "UNIT",
+      ...tsJestPresets,
+      runner: "@codejedi365/jest-serial-runner",
+      setupFilesAfterEnv: ["jest-extended/all", "./tests/setupTests.ts"],
+      testPathIgnorePatterns: ["/node_modules/"],
+      testMatch: ["<rootDir>/src/**/__tests__/**/*.spec.ts"]
+    },
+    {
+      displayName: "SYSTEM",
+      ...tsJestPresets,
+      runner: "@codejedi365/jest-serial-runner",
+      setupFilesAfterEnv: ["jest-extended/all", "./tests/setupTests.ts"],
+      testMatch: ["<rootDir>/tests/**/*.sys-test.ts"]
+    }
+  ]
 };
