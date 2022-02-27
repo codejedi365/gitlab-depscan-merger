20adf08ccc8530c0e9ea96a91cad3c9d9a48cfb6
codejedi365 <codejedi365@gmail.com>
2022-02-27 03:36:05 +0000

    chore(devcontainer): fix missing dependency of `postattach.sh`

--- .devcontainer/postattach.sh
+++ .devcontainer/postattach.sh
@@ -4,13 +4,66 @@
 
 [ -z "$LOG_PREFIX" ] && LOG_PREFIX="[post-attach]"
 
-PROJECT_DIR=""
-if ! PROJECT_DIR="$(git rev-parse --show-toplevel)"; then
-    printf >&2 '%s\n' "$LOG_PREFIX ERROR: something bad happened."
-fi
+# as opposed to echo, interpret C escape sequences properly in all envs
+replay() {
+    printf '%b\n' "$*"
+}
 
-# shellcheck source=.husky/hook-utils.sh
-. "$PROJECT_DIR/.husky/hook-utils.sh"
+# Print to stdout as messages with a prefix of $LOG_PREFIX
+log() {
+    replay "$@" | awk -v "PREFIX=$LOG_PREFIX" -F '\\\\n' '{print PREFIX " " $1}'
+}
+
+# Print to stderr as messages with a prefix of $LOG_PREFIX
+error() {
+    replay "$@" | awk >&2 -v "PREFIX=$LOG_PREFIX" -F '\\\\n' '{print PREFIX " " $1}'
+}
+
+# Prints and runs command
+explicit_run_cmd() {
+    cmd="$*"
+    log "$> $cmd"
+    eval "$cmd"
+}
+
+# Function to configure git repository to include project `.gitconfig`
+config_git_project_gitconfig() {
+  output="$(git config --local --get include.path || true)"
+  if [ "$output" = "../.gitconfig" ]; then
+    unset -v output
+    return 0 # As desired, return silently
+  fi
+  unset -v output
+  if [ -f "../.gitconfig" ] && ! git config --local include.path "../.gitconfig"; then
+    error "ERROR: failed to add project .gitconfig to local git configuration."
+    return 1
+  fi
+}
+
+# Function to configure git repository to enforce GPG signed commits
+config_git_commit_signing() {
+  # check if configured properly
+  if ! output="$(git config --get commit.gpgsign 2>/dev/null)"; then
+    error "ERROR: missing commit.gpgsign setting in git config."
+    return 1
+  elif ! [ "$output" = "true" ]; then
+    error "ERROR: commit.gpgsign must be set to true for project."
+    return 1
+  fi
+  if ! git config --get user.signingkey 1>/dev/null 2>&1; then
+    log "==============================================================="
+    log "                   USER ACTION REQUIRED!"
+    log "---------------------------------------------------------------"
+    log "GPG commit signing is required for this repository! Please"
+    log "configure your repository with the following command:"
+    log "" # prefixed-newline
+    log "    git config --local user.signingkey <GPG_KEY_ID>"
+    log "" # prefixed-newline
+    log "==============================================================="
+  else
+    log "Signature exists: user.signingkey=$(git config --get user.signingkey)"
+  fi
+}
 
 # for devcontainers, its best to use ssh to contact origin since HTTPS requires user auth
 # which is not easy to setup from within a container
@@ -56,6 +109,19 @@ config_git_ssh_origin() {
     unset -v ORIGIN_ORIGINAL_URL ORIGIN_SSH_URL
 }
 
+# Unset all functions/vars this file creates
+cleanup() {
+  unset -v LOG_PREFIX VALID_GIT_CONFIG
+  unset -f cleanup replay log error explicit_run_cmd \
+           config_git_project_gitconfig config_git_commit_signing \
+           config_git_ssh_origin
+}
+
+
+# ------------------
+# MAIN
+# ------------------
+
 VALID_GIT_CONFIG="true"
 
 # [1] Validate git configuration is intact
@@ -72,6 +138,4 @@ if [ "$VALID_GIT_CONFIG" = "true" ]; then
     log "SUCCESS: verified git config"
 fi
 
-unset -v PROJECT_DIR VALID_GIT_CONFIG
-unset -f config_git_ssh_origin
 cleanup
